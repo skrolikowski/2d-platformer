536f8c89fc9230c6b4e3303dae8dbaa6dab098e1
Shane Krolikowski <skrolikowski@gmail.com>
2020-07-11 10:18:15 -0700

    adding Tiled files

---
+++ res/maps/tilesets/PFS1/background_obj.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="background_obj" tilewidth="16" tileheight="16" tilecount="4480" columns="64">
+ <image source="background_obj.png" width="1024" height="1120"/>
+</tileset>
---
+++ res/maps/tilesets/PFS1/mainlevbuild.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="mainlevbuild" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="mainlevbuild.png" width="1024" height="1024"/>
+</tileset>
